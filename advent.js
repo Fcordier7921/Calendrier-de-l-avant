9e06138f02ddcaaf7e4614b12cf93b24122194ea
Fcordier7921 <f.cordier@codeur.online>
2020-12-02 16:19:17 +0100

    fuction2

--- advent.js
+++ advent.js
@@ -1,16 +1,22 @@
-let table =['1','2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'];
+let increm = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 
-if (table.length != 0) {
-    let item = table[Math.floor(Math.random() * table.length)];
-    console.log(item)
-    let i;
-    for (i = 1; i < 24; i++) {
+
+    
+    for(i=1; i<25 ; i++) {
+        let item = increm[Math.floor(Math.random() * increm.length)];
         let circle = document.createElement('div');
         circle.className = "jour";
-        circle.id = 'jour' + i;
+        circle.id = 'jour' + item;
+        circle.innerHTML= '<img src="img/'+item+'.png">';
         document.getElementById("calendrier").appendChild(circle);
-        }
-        // delete table[item];
-} else {
+        
+            delete increm[item-1];
+        console.log(increm)  
+    }while(increm.length = 0){
         trace("le tableau est vide");
     }
+    
+    
+
+
+        
