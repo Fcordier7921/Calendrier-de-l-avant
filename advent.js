3c918de7abdbeb237560321252d8cb6a18dc17cb
Fcordier7921 <f.cordier@codeur.online>
2020-12-02 11:55:51 +0100

    fuction1

--- advent.js
+++ advent.js
@@ -1,9 +1,16 @@
+let table =['1','2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'];
 
-let i;
-for (i = 1; i < 24; i++) {
-  let circle = document.createElement('div');
-  circle.className = "jour";
-  circle.id = 'jour'+i;
-  
-  document.getElementById("app").appendChild(circle);
-}
+if (table.length != 0) {
+    let item = table[Math.floor(Math.random() * table.length)];
+    console.log(item)
+    let i;
+    for (i = 1; i < 24; i++) {
+        let circle = document.createElement('div');
+        circle.className = "jour";
+        circle.id = 'jour' + i;
+        document.getElementById("calendrier").appendChild(circle);
+        }
+        // delete table[item];
+} else {
+        trace("le tableau est vide");
+    }
