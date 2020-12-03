ac2f4ba4dc34863cc96a0bc7e002b4a25a6bff58
Fcordier7921 <f.cordier@codeur.online>
2020-12-03 09:01:31 +0100

    fuction4

--- advent.js
+++ advent.js
@@ -1,35 +1,44 @@
 // let increm = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 
 
-    
+
 //     for(i=1; i<25 ; i++) {
 //         let item = increm[Math.floor(Math.random() * increm.length)];
-        
+
 //         let circle = document.createElement('div');
 //         circle.className = "jour";
 //         circle.id = 'jour' + item;
 //         circle.innerHTML= '<img src="img/'+item+'.png">';
 //         document.getElementById("calendrier").appendChild(circle);
 //         increm.splice(item, 1);
-             
+
 //         console.log(increm)  
 //     }while(increm.length = 0){
 //         trace("le tableau est vide");
 //     }
+
+
 var box = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
-var i = box.length
+var i = box.length //nobre de case dans mon tableau
+let day;
 while (i != 1) {
-i--
-let randomNumber = Math.floor(Math.random() * i)
-box.splice(randomNumber, 1);
-console.log(box)
-let circle = document.createElement('div');
-        circle.className = "jour";
-        circle.id = 'jour' + i;
-        circle.innerHTML= '<img src="img/'+i+'.png">';
-        document.getElementById("calendrier").appendChild(circle);
-} 
-    
-
-
-        
+    i--;
+    let randomNumber = Math.floor(Math.random() * i); //
+    var circle = document.createElement('div');
+    circle.id = 'circle' + box[randomNumber];
+    day = box[randomNumber];
+    box.splice(randomNumber, 1);
+    circle.className = "circle";
+    circle.innerHTML = '<a href="" data-toggle="modal" data-target="#m" @click="setModal(message)"><img src="img/' + day + '.png"></a>';
+    document.getElementById("calendrier").appendChild(circle);
+    console.log(circle.id)
+}
+let today = new Date();
+let calDate = new Date('2020-12-' + day);
+calDate.setDate(calDate.getDate());
+if (calDate >= today) {
+    this.modal = 'Hep Hep Hep, on ne triche pas !'
+} else {
+    this.modal = message.content;
+}
+document.getElementById("day").innerHTML=calDate;
